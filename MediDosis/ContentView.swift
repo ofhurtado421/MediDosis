e73d9e75f77d9710ef836ef6e7edb464dd03d99d
fabian <https://ofhurtado421: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@github.com/ofhurtado421/my-application.git>
2026-05-11 21:22:26 -0500

    prueba1

--- MediDosis/ContentView.swift
+++ MediDosis/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world1234!")
         }
         .padding()
     }
