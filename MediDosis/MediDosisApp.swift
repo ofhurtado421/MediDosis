2d0de180dc35aff18acc27b2c1262dbc0397465f
fabian <https://ofhurtado421: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@github.com/ofhurtado421/my-application.git>
2026-05-11 20:23:51 -0500

    Initial Commit

---
+++ MediDosis/MediDosisApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediDosisApp.swift
+//  MediDosis
+//
+//  Created by FABIAN on 11/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MediDosisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
