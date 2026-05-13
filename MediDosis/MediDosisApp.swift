51d8ee1add501623cc490a82295ea1528f0a277a
fabian <https://ofhurtado421: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@github.com/ofhurtado421/my-application.git>
2026-05-12 21:06:39 -0500

    configurarion del arranque de la app
    - La app ahora abre con el TabBar en lugar de la pantalla en blanco que genera Xcode
    - Se conecta la base de datos para que los medicamentos y el historial se guarden en el dispositivo

--- MediDosis/MediDosisApp.swift
+++ MediDosis/MediDosisApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct MediDosisApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
         }
+        .modelContainer(for: [Medication.self, DosageRecord.self])
     }
 }
